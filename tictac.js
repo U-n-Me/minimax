317938af3c68ca0693e69fdcabeb76273730eca8
U-n-Me <up11@iitbbs.ac.in>
2017-07-26 18:56:07 +0530

    Removed test script
    Removed test script from this file to another file named test.js

--- tictac.js
+++ tictac.js
@@ -200,25 +200,3 @@ function isGameOver(board, player = 1){
 }
 
 
-function checkStuff(){
-    const board1 = [
-                    [1, 0, 0, 0],
-                    [1, 1, -1, 0],
-                    [0, -1, 1, 0],
-                    [-1, 1, 0, 1]
-                  ];
-                  
-     const board2 = [
-                       [1, -1, 0],
-                       [0, 1, -1],
-                       [-1, -1, 1]
-                    ];
-    player = 1;
-    opponent = 1 - player;
-    console.log(evaluate(board1));
-    console.log(evaluate(board2));
-    console.log(board2);
-    console.log(getMove(board2, 0));
-}
-
-checkStuff();
